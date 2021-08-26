de1b02dbe7e35699f8313e2f6cd9008b45a36faa
saul <srgx2012@gmail.com>
2021-08-26 09:19:42 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeroesModule } from './heroes/heroes.module';
+import { AppComponent } from './app.component';
+import { ContadorModule } from './Contador/contador.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+   
+ 
+  ],
+  imports: [
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,13 @@
+
+
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  
+})
+export class ListadoComponent  {
+  heroes: string[] =[`Spiderman`,`Ironman`,`Hulk`,`Thor`];
+
+}
